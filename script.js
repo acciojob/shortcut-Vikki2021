121bc50a5f41222c8bee71de0ec7018895023aaa
Vikas Kumar <95131052+Vikki2021@users.noreply.github.com>
2025-02-28 13:44:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function shortcut(s1, s2) {
   // your code here
+	let first = s1[0];
+	let second = s2[0];
+	let ans = first + second;
+
+	if(s1.length!==0 && s2.length!==0) {
+		return ans;
+	}
+	else {
+		return '';
+	}
 }
 
 // Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
